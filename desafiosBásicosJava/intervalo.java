aa32002fa80fa86c2dbe79b1938a33720bd51c0a
TBadWolf <barbara.rce@hotmail.com>
2022-07-13 12:45:24 -0300

    Update intervalo.java

--- desafiosBásicosJava/intervalo.java
+++ desafiosBásicosJava/intervalo.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 import java.util.Scanner;
 
-public class instervalo {
+public class intervalo {
     public static void main(String[] args) throws IOException {
         Scanner leitor = new Scanner(System.in);
         double valor = leitor.nextDouble();
